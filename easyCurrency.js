dd9485215e3f03305bf0f18730268e80824a3d91
Emanuel0-tech <MANELSILVA0830@GMAIL.COM>
2024-08-02 09:01:42 -0300

    Atualizações do HTML e JS, funcionais

--- easyCurrency.js
+++ easyCurrency.js
@@ -1,18 +1,56 @@
+const loginForm = document.getElementById('loginForm');
+const usuarios = [ 
+  { username: 'usuario1', email: 'usuario1@email.com', password: 'senha1' },
+];
 
-document.registro.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const username = document.querySelector(`#username`).value;
-    const email = document.querySelector(`#email`).value;
-    const password = document.querySelector(`#password`).value;
+function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
 
-    let user = {
-            "login": username,
-            "email": email,
-            "pass": password,
-        }
+function validarSenha(senha) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return regex.test(senha);
+}
 
-    // users.unshift(users);
-    // localStorage.setItem(`accessKey`, users);
-    console.log(user)
+function verificarCredenciais(username, password) {
+  return usuarios.some(user => user.username === username && user.password === password);
+}
 
+function armazenarUsuario(usuario) {
+  localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+}
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  
+  if (!username || !email || !password) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+
+  if (!validarEmail(email)) {
+    alert('Email inválido.');
+    return;
+  }
+
+  if (!validarSenha(password)) {
+    alert('Senha fraca. Deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e números.');
+    return;
+  }
+
+  
+  const usuarioEncontrado = usuarios.find(user => user.username === username && user.password === password);
+  if (usuarioEncontrado) {
+  
+    armazenarUsuario(usuarioEncontrado);
+    window.location.href = 'pagina_após_login.html';
+  } else {
+    alert('Usuário ou senha inválidos.');
+  }
 });
